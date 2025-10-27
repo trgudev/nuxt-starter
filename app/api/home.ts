a827be3a70016f052d67269805320c8d931d9172
li <miciili-02@outlook.com>
2025-10-27 14:00:36 +0800

    feat(projects): request

--- app/api/home.ts
+++ app/api/home.ts
@@ -1,3 +1,4 @@
+// 获取首页数据
 export const fetchGetHomePage = () => {
   return useApiFetch<Api.HomeManage.HomePageData>('/api/home')
 }
--- app/api/pricing.ts
+++ app/api/pricing.ts
@@ -1,3 +1,4 @@
+// 获取定价页面数据
 export const fetchGetPricingPage = () => {
   return useApiFetch<Api.PricingManage.PricingPageData>('/api/pricing')
 }
