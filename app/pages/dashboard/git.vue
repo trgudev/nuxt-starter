77b4c800fc75e53954f4c11ed37b594ca05e8cbb
li <miciili-02@outlook.com>
2025-10-27 11:34:43 +0800

    feat(projects): git

---
+++ app/pages/dashboard/git.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'dashboard'
+})
+
+defineOptions({
+  name: 'DashboardGit'
+})
+</script>
+
+<template>
+  <UDashboardPanel id="git">
+    <template #header>
+      <UDashboardNavbar
+        title="Git"
+        :ui="{ right: 'gap-3' }"
+      >
+        <template #leading>
+          <UDashboardSidebarCollapse />
+        </template>
+      </UDashboardNavbar>
+    </template>
+
+    <template #body>
+      <UCard variant="soft">
+        <p>
+          严格遵循
+          <UButton
+            variant="link"
+            target="_blank"
+            href="https://zj-git-guide.readthedocs.io/zh-cn/latest/message/Angular%E6%8F%90%E4%BA%A4%E4%BF%A1%E6%81%AF%E8%A7%84%E8%8C%83/"
+          >
+            Angular提交信息规范
+          </UButton>
+        </p>
+
+        <p>
+          集成了
+          <UBadge variant="soft">
+            simple-git-hooks
+          </UBadge>，
+          会在提交代码之前进行代码检查，若有错误会提示用户进行修正。
+        </p>
+
+        <p>
+          建议使用命令行提交，首先将文件放到暂存区，然后运行
+          <UBadge variant="soft">
+            pnpm commit
+          </UBadge>。
+        </p>
+      </UCard>
+    </template>
+  </UDashboardPanel>
+</template>
---
+++ prettier.config.mjs
@@ -0,0 +1,22 @@
+// @see: https://www.prettier.cn
+module.exports = {
+  printWidth: 130,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'none',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'auto',
+  rangeStart: 0,
+  rangeEnd: Infinity
+}
