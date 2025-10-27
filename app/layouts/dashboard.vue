95516bba24f46af9d6155f587bba2b5bff7b3bb0
li <miciili-02@outlook.com>
2025-10-27 10:50:23 +0800

    feat(projects): add menu

--- app/layouts/dashboard.vue
+++ app/layouts/dashboard.vue
@@ -19,6 +19,46 @@ const links = [
     onSelect: () => {
       open.value = false
     }
+  },
+  {
+    label: 'Icon',
+    icon: 'i-lucide-star',
+    to: '/dashboard/icon',
+    onSelect: () => {
+      open.value = false
+    }
+  },
+  {
+    label: 'Request',
+    icon: 'carbon:data-1',
+    to: '/dashboard/request',
+    onSelect: () => {
+      open.value = false
+    }
+  },
+  {
+    label: 'Form',
+    icon: 'fluent:form-20-regular',
+    to: '/dashboard/form',
+    onSelect: () => {
+      open.value = false
+    }
+  },
+  {
+    label: 'Git',
+    icon: 'iconoir:git',
+    to: '/dashboard/git',
+    onSelect: () => {
+      open.value = false
+    }
+  },
+  {
+    label: 'Docker',
+    icon: 'mdi:docker',
+    to: '/dashboard/docker',
+    onSelect: () => {
+      open.value = false
+    }
   }
 ] satisfies NavigationMenuItem[]
 
