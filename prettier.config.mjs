4e96f0e3b8dead495194e75c413aae32acc31d9c
li <miciili-02@outlook.com>
2025-10-29 11:37:07 +0800

    feat(projects): prettier

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,22 +1,17 @@
-// @see: https://www.prettier.cn
 export default {
   printWidth: 130,
   tabWidth: 2,
   useTabs: false,
-  semi: true,
-  singleQuote: true,
   jsxSingleQuote: true,
   quoteProps: 'as-needed',
-  trailingComma: 'none',
   bracketSpacing: true,
   bracketSameLine: false,
   arrowParens: 'avoid',
-  requirePragma: false,
-  insertPragma: false,
   proseWrap: 'preserve',
   htmlWhitespaceSensitivity: 'css',
   vueIndentScriptAndStyle: false,
   endOfLine: 'auto',
-  rangeStart: 0,
-  rangeEnd: Infinity
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none'
 }
