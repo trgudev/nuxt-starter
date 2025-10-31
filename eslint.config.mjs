c2e32ad6e72ae0ae4344a66ada72d56a711f4b68
li <miciili-02@outlook.com>
2025-10-31 11:06:53 +0800

    style(projects): lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
   rules: {
+    'semi': ['error', 'never'],
     'vue/html-self-closing': 'error',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
