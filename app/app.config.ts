729a3b35fcf623b3fecd680d9da400de6f63c9d2
li <miciili-02@outlook.com>
2026-01-08 13:03:30 +0800

    feat(projects): ui config

--- app/app.config.ts
+++ app/app.config.ts
@@ -3,6 +3,19 @@ export default defineAppConfig({
     colors: {
       primary: 'green',
       neutral: 'slate'
+    },
+    button: {
+      slots: { base: 'cursor-pointer' }
+    },
+    selectMenu: {
+      slots: {
+        trailingIcon: 'group-data-[state=open]:rotate-180 transition-transform duration-200'
+      }
+    },
+    select: {
+      slots: {
+        trailingIcon: 'group-data-[state=open]:rotate-180 transition-transform duration-200'
+      }
     }
   }
 })
